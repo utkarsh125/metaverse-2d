6e654d288e32c5110e69cffa39838be50eae030a
Utkarsh Pandey <utk2665@gmail.com>
2025-05-22 22:48:27 +0530

    Added Routes in /apps/http

---
+++ metaverse/apps/http/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import { router } from "./routes/v1"
+
+const app = express();
+
+app.use("/api/v1", router)
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log("Listening on PORT 3000")
+})
---
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -0,0 +1,35 @@
+// this is going to export our root router
+
+import { Router } from "express";
+
+//any time a request comes to "/api/v1" it will be handled by this router
+
+export const router = Router();
+
+router.post("/signup", (req, res) => {
+    res.json({
+        message: "Signup"
+    })
+})
+
+router.post("/signin", (req, res) => {
+    res.json({
+        message: "Signin"
+    })
+})
+
+
+router.get("/elements", (req, res) => {
+
+})
+
+router.get("/avatars", (req, res) => {
+    
+})
+
+// any request that starts with /user will be handled by userRouter
+router.use("/user", userRouter)
+// any request that starts with /space will be handled by spaceRouter
+router.use("/space", spaceRouter)
+// any request that starts with /admin will be handled by adminRouter
+router.use("/admin", adminRouter)
