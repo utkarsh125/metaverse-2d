2d147f8e07e17a211f52b242718f944a1b8a2606
Utkarsh Pandey <utk2665@gmail.com>
2025-06-13 22:40:41 +0530

    fix: /v1/index.ts elements, avatars

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -106,12 +106,24 @@ router.post("/signin", async(req, res) => {
 })
 
 
-router.get("/elements", (req, res) => {
-
+router.get("/elements", async (req, res) => {
+    const elements = await client.element.findMany()
+    res.json({elements: elements.map(e => ({
+        id: e.id,
+        imageUrl: e.imageUrl,
+        width: e.width,
+        height: e.height,
+        static: e.static
+    }))})
 })
 
-router.get("/avatars", (req, res) => {
-
+router.get("/avatars", async(req, res) => {
+    const avatars = await client.avatar.findMany()
+    res.json({avatars: avatars.map(x => ({
+        id: x.id,
+        imageUrl: x.imageUrl,
+        name: x.name,
+    }))})
 })
 
 // any request that starts with /user will be handled by userRouter
